aaf0e700aa746f704f40ac1e5e05723a50cbc9ed
phucleeuwu <125681538+phucleeuwu@users.noreply.github.com>
2025-07-04 13:21:20 +0700

    add sketchybar config

---
+++ sketchybar/helpers/default_font.lua
@@ -0,0 +1,12 @@
+return {
+  text = "Maple Mono", -- Used for text
+  numbers = "Maple Mono", -- Used for numbers
+  weather = "Symbols Nerd Font",
+  style_map = {
+    ["Regular"] = "Regular",
+    ["Semibold"] = "Semibold",
+    ["Bold"] = "Bold",
+    ["Heavy"] = "Heavy",
+    ["Black"] = "Black",
+  },
+}
