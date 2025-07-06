7da356158b30cc934aa53713d759f51987d824cd
phucisstupid <125681538+phucleeuwu@users.noreply.github.com>
2025-07-06 15:14:53 +0700

    remove githead yazi

--- yazi/init.lua
+++ yazi/init.lua
@@ -1,5 +1,4 @@
 require("full-border"):setup()
 require("git"):setup()
 require("yatline"):setup(0, require("yatline-catppuccin"):setup("mocha"))
-require("yatline-githead"):setup()
 require("relative-motions"):setup({ show_numbers = "relative_absolute" })
