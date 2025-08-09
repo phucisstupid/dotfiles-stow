48482ce8425d0c6e6ce2aa89e64629b4688cc9a0
phucisstupid <125681538+phucleeuwu@users.noreply.github.com>
2025-08-09 22:14:15 +0700

    update sketchybar config better than ever

--- sketchybar/bar.lua
+++ sketchybar/bar.lua
@@ -1,16 +1,11 @@
 local colors = require("colors").sections.bar
 
-sbar.bar {
-  topmost = "window",
-  height = 41,
-  notch_display_height = 41,
-  padding_right = 12,
-  padding_left = 12,
-  margin = -1,
-  corner_radius = 0,
-  y_offset = -1,
-  blur_radius = 20,
-  border_color = colors.border,
-  border_width = 1,
-  color = colors.bg,
-}
+sbar.bar({
+	topmost = "window",
+	height = 30,
+	notch_display_height = 30,
+	padding_right = 5,
+	padding_left = 5,
+	border_color = colors.border,
+	color = colors.bg,
+})
