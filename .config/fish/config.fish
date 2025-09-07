e6f609051ef7bf683379f812541d2ac6f6457dc9
phucisstupid <125681538+phucisstupid@users.noreply.github.com>
2025-09-07 22:32:52 +0700

    update fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,11 +1,10 @@
-#starship init fish | source
 zoxide init fish | source
-oh-my-posh init fish --config (brew --prefix oh-my-posh)/themes/pure.omp.json | source
+starship init fish | source
 
 function y
     set tmp (mktemp -t "yazi-cwd.XXXXXX")
     yazi $argv --cwd-file="$tmp"
-    if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+    if read -z cwd <"$tmp"; and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
         builtin cd -- "$cwd"
     end
     rm -f -- "$tmp"
