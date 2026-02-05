1777fe91263a47f96d5fa483f93ba02815acf46e
phucisstupid <125681538+phucisstupid@users.noreply.github.com>
2026-02-05 23:02:07 +0700

    up

--- .config/yazi/init.lua
+++ .config/yazi/init.lua
@@ -1,4 +1,12 @@
+local catppuccin_theme = require("yatline-catppuccin"):setup("mocha")
 require("full-border"):setup()
 require("git"):setup()
-require("yatline"):setup(0, require("yatline-catppuccin"):setup("mocha"))
-require("relative-motions"):setup({ show_numbers = "relative_absolute" })
+require("yatline-githead"):setup({
+	theme = catppuccin_theme,
+})
+require("yatline"):setup({
+	theme = catppuccin_theme,
+})
+require("relative-motions"):setup({
+	show_numbers = "relative_absolute",
+})
