80c6215d386865105079af028ea439be8b338062
phucisstupid <125681538+phucisstupid@users.noreply.github.com>
2026-05-25 12:33:27 +0700

    update yazi config

--- .config/yazi/init.lua
+++ .config/yazi/init.lua
@@ -1,6 +1,8 @@
 local catppuccin_theme = require("yatline-catppuccin"):setup("mocha")
 require("full-border"):setup()
-require("git"):setup()
+require("git"):setup({
+  order = 1500,
+})
 require("yatline"):setup({
 	theme = catppuccin_theme,
 })
